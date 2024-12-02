c6938fd38bda2e46fd0ee4f6fa5558516a0bcebc
Chongyu-Liu <41882094+lcy0604@users.noreply.github.com>
2024-12-02 14:10:50 +0800

    Update erase_syn.py

--- datasets/erase_syn.py
+++ datasets/erase_syn.py
@@ -12,105 +12,6 @@ from shapely.geometry import Polygon
 # import .transforms as T
 from . import transforms as T
 
-def draw_border_map(polygon, canvas, mask_ori, mask):
-    polygon = np.array(polygon)
-    assert polygon.ndim == 2
-    assert polygon.shape[1] == 2
-
-    ### shrink box ###
-    polygon_shape = Polygon(polygon)
-    distance = polygon_shape.area * \
-        (1 - np.power(0.95, 2)) / polygon_shape.length
-    subject = [tuple(l) for l in polygon]
-    padding = pyclipper.PyclipperOffset()
-    padding.AddPath(subject, pyclipper.JT_ROUND, pyclipper.ET_CLOSEDPOLYGON)
-    padded_polygon = np.array(padding.Execute(-distance)[0])
-    cv2.fillPoly(mask, [padded_polygon.astype(np.int32)], 1.0)
-    ### shrink box ###
-
-    cv2.fillPoly(mask_ori, [polygon.astype(np.int32)], 1.0)
-
-    polygon = padded_polygon
-    polygon_shape = Polygon(padded_polygon)
-    distance = polygon_shape.area * \
-        (1 - np.power(0.4, 2)) / polygon_shape.length
-    subject = [tuple(l) for l in polygon]
-    padding = pyclipper.PyclipperOffset()
-    padding.AddPath(subject, pyclipper.JT_ROUND, pyclipper.ET_CLOSEDPOLYGON)
-    padded_polygon = np.array(padding.Execute(distance)[0])
-
-    xmin = padded_polygon[:, 0].min()
-    xmax = padded_polygon[:, 0].max()
-    ymin = padded_polygon[:, 1].min()
-    ymax = padded_polygon[:, 1].max()
-    width = xmax - xmin + 1
-    height = ymax - ymin + 1
-
-    polygon[:, 0] = polygon[:, 0] - xmin
-    polygon[:, 1] = polygon[:, 1] - ymin
-
-    xs = np.broadcast_to(
-        np.linspace(0, width - 1, num=width).reshape(1, width), (height, width))
-    ys = np.broadcast_to(
-        np.linspace(0, height - 1, num=height).reshape(height, 1), (height, width))
-
-    distance_map = np.zeros(
-        (polygon.shape[0], height, width), dtype=np.float32)
-    for i in range(polygon.shape[0]):
-        j = (i + 1) % polygon.shape[0]
-        # import pdb;pdb.set_trace()
-        absolute_distance = coumpute_distance(xs, ys, polygon[i], polygon[j])
-        distance_map[i] = np.clip(absolute_distance / distance, 0, 1)
-    distance_map = distance_map.min(axis=0)
-
-    xmin_valid = min(max(0, xmin), canvas.shape[1] - 1)
-    xmax_valid = min(max(0, xmax), canvas.shape[1] - 1)
-    ymin_valid = min(max(0, ymin), canvas.shape[0] - 1)
-    ymax_valid = min(max(0, ymax), canvas.shape[0] - 1)
-    canvas[ymin_valid:ymax_valid + 1, xmin_valid:xmax_valid + 1] = np.fmax(
-        1 - distance_map[
-            ymin_valid-ymin:ymax_valid-ymax+height,
-            xmin_valid-xmin:xmax_valid-xmax+width],
-        canvas[ymin_valid:ymax_valid + 1, xmin_valid:xmax_valid + 1])
-
-def coumpute_distance(xs, ys, point_1, point_2):
-    '''
-    compute the distance from point to a line
-    ys: coordinates in the first axis
-    xs: coordinates in the second axis
-    point_1, point_2: (x, y), the end of the line
-    '''
-    height, width = xs.shape[:2]
-    square_distance_1 = np.square(
-        xs - point_1[0]) + np.square(ys - point_1[1])
-    square_distance_2 = np.square(
-        xs - point_2[0]) + np.square(ys - point_2[1])
-    square_distance = np.square(
-        point_1[0] - point_2[0]) + np.square(point_1[1] - point_2[1])
-
-    cosin = (square_distance - square_distance_1 - square_distance_2) / \
-        (2 * np.sqrt(square_distance_1 * square_distance_2))
-    square_sin = 1 - np.square(cosin)
-    square_sin = np.nan_to_num(square_sin)
-    result = np.sqrt(square_distance_1 * square_distance_2 *
-                     square_sin / square_distance)
-
-    result[cosin < 0] = np.sqrt(np.fmin(
-        square_distance_1, square_distance_2))[cosin < 0]
-    # extend_line(point_1, point_2, result)
-    return result
-
-def get_seg_map(img, label):
-    canvas = np.zeros(img.shape[:2], dtype = np.float32)
-    mask = np.zeros(img.shape[:2], dtype = np.float32)
-    mask_ori = np.zeros(img.shape[:2], dtype = np.float32)
-    polygons = label
-
-    for i in range(len(polygons)):
-        draw_border_map(polygons[i], canvas, mask_ori, mask=mask)
-    return canvas, mask, mask_ori
-
-
 class EraseData(Dataset):
     def __init__(self, data_root, transform, phase):
         image_folder = os.path.join(data_root, 'image')
@@ -119,7 +20,6 @@ class EraseData(Dataset):
 
         structure_folder = os.path.join(data_root, 'structure_im')
         structure_lbl_folder = structure_folder
-        # structure_lbl_folder = os.path.join(data_root, 'structure_lbl')
 
         image_names = os.listdir(image_folder)
         image_names.sort()
@@ -142,8 +42,6 @@ class EraseData(Dataset):
         lbl1 = cv2.cvtColor(label, cv2.COLOR_BGR2RGB)
         label = Image.fromarray(cv2.cvtColor(label, cv2.COLOR_BGR2RGB))
 
-        # image = Image.open(self.image_paths[index]).convert('RGB')
-        # label = Image.open(self.label_paths[index]).convert('RGB')
         mask_gt = Image.open(self.mask_paths[index]).convert('1')
         mask = Image.new('1', image.size, 1)
 
@@ -152,12 +50,8 @@ class EraseData(Dataset):
         mask1[index1] = 255
         index1 = np.where(mask1 <= 5)
         mask1[index1] = 0
-        # index1 = np.where(mask1 != 0)
-        # mask1[index1] = 255
-        # import pdb;pdb.set_trace()
-        # mask1 = np.greater(np.mean(np.abs(np.array(im1).astype(np.float32) - np.array(lbl1).astype(np.float32)), axis=-1),15).astype(np.uint8)
-        mask1 = Image.fromarray(np.uint8(mask1)).convert('1')
-        # import pdb;pdb.set_trace()
+
+        mask1 = Image.fromarray(np.uint8(255 - mask1)).convert('1')
 
         structure_im = cv2.imread(self.structure_im_paths[index])
         try:
@@ -168,44 +62,7 @@ class EraseData(Dataset):
         structure_lbl = Image.fromarray(cv2.cvtColor(structure_lbl, cv2.COLOR_BGR2RGB))
 
         data = {'image': image, 'label': label, 'mask': mask, 'mask_gt': mask1, 'structure_im': structure_im, 'structure_lbl': structure_lbl, 'soft_mask': mask1}
-        # structure_im = Image.open(self.structure_im_paths[index]).convert('RGB')
-        # structure_lbl = Image.open(self.structure_lbl_paths[index]).convert('RGB')
 
-        # stroke_mask = get_stroke_mask(image, label)
-        # import pdb;pdb.set_trace()
-        # gt_path = self.image_paths[index].replace('image', 'gt').replace('jpg','txt')
-        # gt_path = self.image_paths[index].replace('image', 'detect_re').replace('jpg','txt')
-        # img = np.array(image)
-        # bboxes = get_anno(img, gt_path)
-
-        # gt_instance = np.zeros(img.shape[0:2], dtype='uint8')
-
-        # if len(bboxes) > 0:
-        #     for i in range(len(bboxes)):
-        #         bboxes[i] = np.reshape(bboxes[i] * ([img.shape[1], img.shape[0]] * (bboxes[i].shape[0] //2)),
-        #                                 (bboxes[i].shape[0] // 2, 2)).astype('int32')
-        #     for i in range(len(bboxes)):
-        #         cv2.drawContours(gt_instance, [bboxes[i]], -1, i + 1, -1)
-
-        # gt_text= gt_instance.copy()
-        # gt_text[gt_text > 0] = 1
-
-        # gt_text = Image.fromarray((1 - gt_text)*255)
-        # gt_text = gt_text.convert('L')
-        # # import pdb;pdb.set_trace()
-
-        # canvas, shrink_mask, mask_ori = get_seg_map(img, bboxes)
-        # soft_mask = canvas + mask_ori
-        # index_mask = np.where(soft_mask > 1)
-        # soft_mask[index_mask] = 1
-
-        # # import pdb;pdb.set_trace()
-        # soft_mask = Image.fromarray((1 - soft_mask) * 255)
-        # soft_mask = soft_mask.convert('L')
-        # # import pdb;pdb.set_trace()
-
-        # data = {'image': image, 'label': label, 'mask': mask, 'mask_gt': gt_text, 'structure_im': structure_im, 'structure_lbl': structure_lbl, 'soft_mask': soft_mask}
-        # print(self.transform)
         if not self.transform is None:
             data = self.transform(data)
 
@@ -228,11 +85,7 @@ def get_stroke_mask(image, label):
     ma[index] = 255
     index = np.where(ma <= 25)
     ma[index] = 0
-    # ma = np.greater(np.mean(np.abs(np.array(im).astype(np.float32) - np.array(lbl).astype(np.float32)), axis=-1),25).astype(np.uint8)
-    # ma = np.greater(np.mean(np.abs(im.astype(np.float32) - lbl.astype(np.float32)), axis=-1), 30).astype(np.uint8) # Threshold is set to 25
 
-    # ma = np.expand_dims(ma, axis=2).astype(np.float32)
-    # import pdb;pdb.set_trace()
     ma = Image.fromarray(ma)
     ma = ma.convert('RGB')
     ma.save('1.jpg')
